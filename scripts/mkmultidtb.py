b5a283331fa0a6f4c735bf72291bf95c7c710752
fewtarius <fewtarius@jelos.org>
2023-03-08 12:33:35 -0500

    Work on multi dtb.

--- scripts/mkmultidtb.py
+++ scripts/mkmultidtb.py
@@ -32,7 +32,8 @@ DTBS['rk3566'] = OrderedDict([('rk3566-rg353p-linux', '#_saradc_ch1=852'),
 			      ('rk3566-rg503-linux', '#_saradc_ch1=1023')])
 
 DTBS['rk3588'] = OrderedDict([('rk3588s-orangepi-5', '#_saradc_ch1=001'),
-			      ('rk3588s-9tripod-linux', '#_saradc_ch1=002')])
+                              ('rk3588-rock-5b', '#_saradc_ch1=002'),
+			      ('rk3588s-9tripod-linux', '#_saradc_ch1=4095')])
 
 def main():
 
